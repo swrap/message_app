bc6bf626559b3eb18831e5fc157b75e8ba76917c
swrap <jesse.saranwrap@gmail.com>
2016-12-28 01:07:20 -0500

    What Changed:  - Adding in all files

---
+++ sample/src/main/java/com/klinker/android/messaging_sample/MainActivity.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright 2014 Jacob Klinker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klinker.android.messaging_sample;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Telephony;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.android.mms.service_alt.MmsConfigManager;
+import com.klinker.android.logger.Log;
+import com.klinker.android.logger.OnLogListener;
+import com.klinker.android.send_message.ApnUtils;
+import com.klinker.android.send_message.Message;
+import com.klinker.android.send_message.Transaction;
+import com.klinker.android.send_message.Utils;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private Settings settings;
+
+    private Button setDefaultAppButton;
+    private Button selectApns;
+    private EditText fromField;
+    private EditText toField;
+    private EditText messageField;
+    private ImageView imageToSend;
+    private Button sendButton;
+    private RecyclerView log;
+
+    private LogAdapter logAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean("request_permissions", true) &&
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            startActivity(new Intent(this, PermissionActivity.class));
+            finish();
+            return;
+        }
+
+        setContentView(R.layout.activity_main);
+
+        initSettings();
+        initViews();
+        initActions();
+        initLogging();
+    }
+
+    private void initSettings() {
+        settings = Settings.get(this);
+
+        if (TextUtils.isEmpty(settings.getMmsc())) {
+            initApns();
+        }
+    }
+
+    private void initApns() {
+        ApnUtils.initDefaultApns(this, new ApnUtils.OnApnFinishedListener() {
+            @Override
+            public void onFinished() {
+                settings = Settings.get(MainActivity.this, true);
+            }
+        });
+    }
+
+    private void initViews() {
+        setDefaultAppButton = (Button) findViewById(R.id.set_as_default);
+        selectApns = (Button) findViewById(R.id.apns);
+        fromField = (EditText) findViewById(R.id.from);
+        toField = (EditText) findViewById(R.id.to);
+        messageField = (EditText) findViewById(R.id.message);
+        imageToSend = (ImageView) findViewById(R.id.image);
+        sendButton = (Button) findViewById(R.id.send);
+        log = (RecyclerView) findViewById(R.id.log);
+    }
+
+    private void initActions() {
+        if (Utils.isDefaultSmsApp(this)) {
+            setDefaultAppButton.setVisibility(View.GONE);
+        } else {
+            setDefaultAppButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setDefaultSmsApp();
+                }
+            });
+        }
+
+        selectApns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initApns();
+            }
+        });
+
+        fromField.setText(Utils.getMyPhoneNumber(this));
+        toField.setText(Utils.getMyPhoneNumber(this));
+
+        imageToSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleSendImage();
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
+        log.setHasFixedSize(false);
+        log.setLayoutManager(new LinearLayoutManager(this));
+        logAdapter = new LogAdapter(new ArrayList<String>());
+        log.setAdapter(logAdapter);
+    }
+
+    private void initLogging() {
+        Log.setDebug(true);
+        Log.setPath("messenger_log.txt");
+        Log.setLogListener(new OnLogListener() {
+            @Override
+            public void onLogged(String tag, String message) {
+                //logAdapter.addItem(tag + ": " + message);
+            }
+        });
+    }
+
+    private void setDefaultSmsApp() {
+        setDefaultAppButton.setVisibility(View.GONE);
+        Intent intent =
+                new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
+                getPackageName());
+        startActivity(intent);
+    }
+
+    private void toggleSendImage() {
+        if (imageToSend.isEnabled()) {
+            imageToSend.setEnabled(false);
+            imageToSend.setAlpha(0.3f);
+        } else {
+            imageToSend.setEnabled(true);
+            imageToSend.setAlpha(1.0f);
+        }
+    }
+
+    public void sendMessage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !android.provider.Settings.System.canWrite(this)) {
+            new AlertDialog.Builder(this)
+                    .setMessage(com.klinker.android.send_message.R.string.write_settings_permission)
+                    .setPositiveButton(com.klinker.android.send_message.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                            intent.setData(Uri.parse("package:" + getPackageName()));
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                            try {
+                                startActivity(intent);
+                            } catch (Exception e) {
+                                Log.e("MainActivity", "error starting permission intent", e);
+                            }
+                        }
+                    })
+                    .show();
+            return;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                com.klinker.android.send_message.Settings sendSettings = new com.klinker.android.send_message.Settings();
+                sendSettings.setMmsc(settings.getMmsc());
+                sendSettings.setProxy(settings.getMmsProxy());
+                sendSettings.setPort(settings.getMmsPort());
+                sendSettings.setUseSystemSending(true);
+
+                Transaction transaction = new Transaction(MainActivity.this, sendSettings);
+
+                Message message = new Message(messageField.getText().toString(), toField.getText().toString());
+
+                if (imageToSend.isEnabled()) {
+                    message.setImage(BitmapFactory.decodeResource(getResources(), R.drawable.android));
+                }
+
+                transaction.sendNewMessage(message, Transaction.NO_THREAD_ID);
+            }
+        }).start();
+    }
+
+}
